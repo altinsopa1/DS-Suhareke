becbfe730dc62fabb0b706737615cd1ea7f54196
altinsopa1 <altin.sopa@hotmail.com>
2025-04-14 19:16:25 +0200

    7;16

---
+++ Module 29/easy.js
@@ -0,0 +1,2 @@
+var favouriteBook = ["Dheu eshte flori","GU GU PICIGU","Ditari Ana Frankut"];
+console.log(favouriteBook[2]);
---
+++ Module 30/main.js
@@ -0,0 +1,19 @@
+var input = document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text  = document.getElementById('text_id');
+//button.onclick = function(){
+    //Text.innerHTML = input.value;
+//}
+var input1 =document.querySelector('#input1_id');
+var input2 =document.querySelector('#input2_id');
+var btn2 =document.querySelector('#btn2_id');
+var ans =document.querySelector('#answer');
+
+btn2.addEventListener("click",function(){
+    ans.innerHTML = parseInt(input1.value) + parseInt(input2.value);
+    });
+
+    if(input.value > 10)
+    {
+        text.innerHTML
+    }
