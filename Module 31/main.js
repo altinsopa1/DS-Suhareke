f01519a173c18be8e6c02d64f6cced3428d55467
altinsopa1 <altin.sopa@hotmail.com>
2025-05-05 19:20:55 +0200

    7:20

---
+++ Module 31/main.js
@@ -0,0 +1,71 @@
+/*var input =document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text=document.getElementById('teksti_id');
+
+button.onclick=function(){
+    text.innerHTML=input.value ;
+}*/
+
+/*var text="The best school in the world is Digital School!";
+var result=text.search("Digital School");
+document.getElementById("result1").innerHTML = result;
+*/
+
+/*var text="The best school in the world is Digital School!";
+var result=text.search(/Digital School/);
+document.getElementById("result2").innerHTML=result;
+*/
+
+/*var text="The best school in the world is digital school!";
+var result=text.replace(/digital school/,"another school");
+document.getElementById("result3").innerHTML=result;
+*/
+
+/*var text="abcdef";
+var regex=new RegExp('abc');
+document.getElementById("result4").innerHTML=regex.test(text);*/
+
+/*var text="my school is the best school in the world ";
+var regex=/school/g;
+document.getElementById("result5").innerHTML=text.match(regex);*/
+
+/*var text="Digital school is the best school in the world ";
+var regex=/i/g;
+document.getElementById("result6").innerHTML=text.match(regex);*/
+
+/*var text="Digital school is the best school in the world ";
+var regex=/[abc]/g;
+document.getElementById("result7").innerHTML=text.match(regex);
+*/
+
+//var text="Digital school is in top 10 best schools in the world ";
+//var regex=/[0-9]/g;
+//document.getElementById("result8").innerHTML=text.match(regex);
+
+/*var text="My school is the best school in the world!";
+var regex=/(top|best|cool)/g;
+document.getElementById("result9").innerHTML=text.match(regex);*/
+
+/*var text="100 percent!";
+var regex=/\d/g;
+document.getElementById("result10").innerHTML=text.match(regex);*/
+
+/*var text="My school is the best school in the world";
+var regex=/\s/g;
+document.getElementById("result11").innerHTML=text.match(regex);*/
+
+/*var text="Heeey, how are you";
+var regex=/e+/g;
+document.getElementById("result12").innerHTML=text.match(regex);*/
+
+//*var text="so, I hope we will see each other soon again...";
+//var regex=/so*/
+//document.getElementById("result13").innerHTML=text.match(regex);*/
+
+/*var text="hey,hi,hiii";
+var regex=/h1?/g;
+document.getElementById("result14").innerHtml=text.match(regex);*/
+
+/*var text="hello, hellooo, hellooo!!!, helloooo, hellooooo";
+var regex=/o{3,5}/g;
+document.getElementById("result15").innerHtml=text.match(regex);*/
