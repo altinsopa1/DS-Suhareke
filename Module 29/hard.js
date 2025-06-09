1c3c2a8fca4469c1cc81f4d29ceb293cfa6e5b4b
altinsopa1 <altin.sopa@hotmail.com>
2025-06-09 19:00:42 +0200

    700

--- Module 29/hard.js
+++ Module 29/hard.js
@@ -2,9 +2,9 @@ const strings=["zana","greta","tini","lana"];
 
 const randomIndex = Math.floor(Math.random()*strings.length);
 
-const[extractedWord = '',anotherWord='']=[strings[randomIndex].strings[randomIndex+1]];
+const[extractedWord = '',anotherWord='']=[strings[randomIndex],strings[randomIndex+1]];
 if(extractedWord){
-    console.log("ExtractedWord:tini",extractedWord);
+    console.log("ExtractedWord:",extractedWord);
 
 }else {
     console.log("No valid extracted word found.");
